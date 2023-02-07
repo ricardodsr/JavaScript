d71aadc40013a64177589f1817e1a9c0190d58d9
Ricardo Rouco <ricardo.rouco@gmail.com>
2023-02-07 15:08:59 +0000

    Create Samples.js

---
+++ Samples.js
@@ -0,0 +1,37 @@
+// the hello world program
+console.log('Hello World');
+
+// the hello world program
+alert("Hello, World!");
+
+// the hello world program
+document.write('Hello, World!');
+
+///////////////////////////////////
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+//////////////////////////////////////////////////////////////////
+// store input numbers
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+//add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
+/////////////////////////////////////////////////////////////////
+const num1 = parseInt(prompt('Enter the first number '));
+const num2 = parseInt(prompt('Enter the second number '));
+
+const sum = num1 + num2;
+
+console.log(`The sum of ${num1} and ${num2} is ${sum}`);
